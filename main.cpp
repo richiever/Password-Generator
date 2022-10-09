1d754a6d8e433535367bbee0fa2f167e1a1dbafc
Richie1 <33795818+richiever@users.noreply.github.com>
2022-10-10 01:42:15 +1100

    Created password Generator V1

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <random>// for std::mt19937 and std::random_device
+#include <Windows.h>
+#include <cstdio>
+#include <conio.h>
+
+
+// gets amount of numbers
+void GetChosenNumbs(int& x)
+{
+	int y{};
+	std::cout << "Enter How long your password should be(0-16): ";
+	std::cin >> y;
+	assert(y < 17 && "This number is too large!");
+	assert(y > 0 && "This number is too small or not an integer!");
+	x = y;
+}
+
+
+// Randomizes between ASCII chars
+char randomizer()
+{
+	std::mt19937 mt{ std::random_device{}() };
+	std::uniform_int_distribution<> numberGen{ 33, 127 };
+
+	return numberGen(mt);
+}
+
+
+int main()
+{
+
+	std::vector<char> base{};
+	int x{};
+	std::string passwordBase{};
+	GetChosenNumbs(x);
+	std::cout << x;
+
+	for (int i{ 0 }; i < x; ++i)
+		base.push_back(randomizer());
+
+	for (int j{ 0 }; j < base.size(); ++j)
+		passwordBase += base[j];
+	std::cout << "\nYour password is: " << passwordBase << "\n";
+
+	OpenClipboard(0);
+	EmptyClipboard();
+	const char* op{ passwordBase.c_str() };
+	const size_t ln = strlen(op) + 1;
+	HGLOBAL h = GlobalAlloc(GMEM_MOVEABLE, ln);
+	memcpy(GlobalLock(h), op, ln);
+	GlobalUnlock(h);
+	SetClipboardData(CF_TEXT, h);
+	CloseClipboard();
+	std::cout << "Your password has been copied to clipboard!" << std::endl;
+
+	system("pause");
+}
